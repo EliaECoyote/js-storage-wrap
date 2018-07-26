079150d212169125d0e162f1f6c8341646c33b8e
Elia Camposilvan <elia.camposilvan@rawfish.com>
2018-07-26 12:44:37 +0200

    fix loadFromStorage test

--- __tests__/storageHelpers-tests.js
+++ __tests__/storageHelpers-tests.js
@@ -2,9 +2,10 @@ import { loadFromStorage } from '../src/storageHelpers';
 
 test('[loadFromStorage] should return null if item is not present', () => {
   const storage = {
-    loadItem: jest.fn()
+    getItem: jest.fn()
   };
   const storageFn = () => storage;
   const item = loadFromStorage({ storageFn, itemName: 'test' });
+  expect(storage.getItem).toHaveBeenCalledTimes(1);
   expect(item).toBeNull();
 });
