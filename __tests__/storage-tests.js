7065d009fd557079fdef6185a86c233f87301fa6
elia_camposilvan <elia.camposilvan@rawfish.it>
2018-08-18 11:38:20 +0200

    e2e tests added for ttl handling

--- __tests__/storage-tests.js
+++ __tests__/storage-tests.js
@@ -1,18 +1,13 @@
 import StorageWrapper from "../src/storage";
 import StorageMock from "../__mocks__/storage";
 
-const date = new Date();
 const testObj = { testString: "foo" };
 const testArray = [1, 2, 3, 4, 5];
-const lifespan = 200;
-
-beforeEach(() => {
-  global.Date = jest.fn(() => date);
-});
-
-afterEach(() => {
-  global.Date = Date;
-});
+const items = [
+  { name: "test_string", value: "foo" },
+  { name: "test_object", value: testObj },
+  { name: "test_array", value: testArray }
+];
 
 test("[StorageWrapper] should return null / false if storage not available", () => {
   global.console = { log: jest.fn() };
@@ -37,13 +32,32 @@ test("[StorageWrapper] should return null / false if storage not available", () 
 test("[StorageWrapper] should return value if present (w/o ttl)", () => {
   global.sessionStorage = new StorageMock();
   const storageWrapper = new StorageWrapper();
-  const items = [
-    { name: "test_string", value: "foo" },
-    { name: "test_object", value: testObj },
-    { name: "test_array", value: testArray }
-  ];
   items.forEach(item => storageWrapper.session.set(item.name, item.value));
-  items.forEach(item => {
-    expect(storageWrapper.session.load(item.name)).toEqual(item.value);
-  });
+  items.forEach(item =>
+    expect(storageWrapper.session.load(item.name)).toEqual(item.value)
+  );
+});
+
+test("[StorageWrapper] should not return value if not present", () => {
+  global.sessionStorage = new StorageMock();
+  const storageWrapper = new StorageWrapper();
+  const names = ["test_string", "test_object", "test_array"];
+  names.forEach(name => expect(storageWrapper.session.load(name)).toBe(null));
+});
+
+test("[StorageWrapper] should return value if present (w ttl)", () => {
+  const date = new Date();
+  date.setMilliseconds(0);
+  global.Date = jest.fn(() => date);
+  const storageWrapper = new StorageWrapper();
+  global.sessionStorage = new StorageMock();
+  items.forEach(item => storageWrapper.session.set(item.name, item.value, 1));
+  items.forEach(item =>
+    expect(storageWrapper.session.load(item.name)).toEqual(item.value)
+  );
+  date.setMilliseconds(2);
+  items.forEach(item =>
+    expect(storageWrapper.session.load(item.name)).toBe(null)
+  );
+  global.Date = Date;
 });
