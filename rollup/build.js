de8a8b0744b487f6dfddf55779caabca2a2b8a0f
elia_camposilvan <elia.camposilvan@rawfish.it>
2019-08-07 20:59:51 +0200

    generating minified build and exporting actual storage fns only for browser imports

--- rollup/build.js
+++ rollup/build.js
@@ -1,27 +1,54 @@
-/* eslint-disable-next-line */
-const rollup = require('rollup');
-/* eslint-disable-next-line */
-const babel = require('rollup-plugin-babel');
+/* eslint-disable-next-line import/no-extraneous-dependencies */
+const { rollup } = require("rollup");
+/* eslint-disable-next-line import/no-extraneous-dependencies */
+const babel = require("rollup-plugin-babel");
+/* eslint-disable-next-line import/no-extraneous-dependencies */
+const { terser } = require("rollup-plugin-terser");
 
-const inputOptions = {
-  input: "src/main.js",
-  plugins: [
-    babel({
-      exclude: "node_modules/**"
-    })
-  ]
-};
-
-const outputOptions = {
-  dir: "dist",
-  file: "bundle.js",
-  format: "cjs"
-};
-
-const build = async () => {
-  const bundle = await rollup.rollup(inputOptions);
+async function buildBrowserBundle() {
+  const inputOptions = {
+    input: "src/main.js",
+    plugins: [babel({ exclude: "node_modules/**" })]
+  };
+  const outputOptions = {
+    file: "dist/browser_bundle.js",
+    format: "cjs"
+  };
+  const bundle = await rollup(inputOptions);
   await bundle.generate(outputOptions);
   await bundle.write(outputOptions);
-};
+}
 
-build();
+async function buildMinifiedBundle() {
+  const inputOptions = {
+    input: "src/main.js",
+    plugins: [babel({ exclude: "node_modules/**" }), terser()]
+  };
+  const outputOptions = {
+    file: "dist/bundle.min.js",
+    compact: true,
+    name: "StorageWrap",
+    format: "iife"
+  };
+  const bundle = await rollup(inputOptions);
+  await bundle.generate(outputOptions);
+  await bundle.write(outputOptions);
+}
+
+async function buildNodeBundle() {
+  const inputOptions = {
+    input: "src/node.js",
+    plugins: [babel({ exclude: "node_modules/**" })]
+  };
+  const outputOptions = {
+    file: "dist/node_bundle.js",
+    format: "cjs"
+  };
+  const bundle = await rollup(inputOptions);
+  await bundle.generate(outputOptions);
+  await bundle.write(outputOptions);
+}
+
+buildBrowserBundle();
+buildMinifiedBundle();
+buildNodeBundle();
